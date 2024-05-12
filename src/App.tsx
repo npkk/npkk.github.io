d0ccb98da4e44b2098f68c2fb2f2c691789d37f3
npkk <kob4.nemu@gmail.com>
2024-05-13 01:07:00 +0900

    format and Close to Back

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,40 @@
-import GridTile from './components/GridTile'
-import ContentModal from './components/ContentModal'
-import './index.css'
-import Logo from '/logo.svg'
-import props from './contents/content'
-import { rootContext, useRootContext } from './contexts/contexts'
+import GridTile from "./components/GridTile";
+import ContentModal from "./components/ContentModal";
+import "./index.css";
+import Logo from "/logo.svg";
+import props from "./contents/content";
+import { rootContext, useRootContext } from "./contexts/contexts";
 
 function App() {
-  const rootCtx = useRootContext()
+	const rootCtx = useRootContext();
 
-  const display = !rootCtx.root ? '' : 'none'
+	const display = !rootCtx.root ? "" : "none";
 
-  function onTileClick(target: string) {
-    rootCtx.setRoot(target)
-  }
+	function onTileClick(target: string) {
+		rootCtx.setRoot(target);
+	}
 
-  return (
-    <>
-      <rootContext.Provider value={rootCtx}>
-        <div id='container'>
-          <div id="grid-container" style={{ display: display }}>
-            <div id='npkk'>
-              <img src={Logo}></img>
-            </div>
-            {
-              props.map((prop) => {
-                return <div id={prop.id} onClick={() => onTileClick(prop.id)}>
-                  <GridTile title={prop.title}></GridTile>
-                </div>
-              })
-            }
-          </div>
-          <ContentModal></ContentModal>
-        </div>
-      </rootContext.Provider>
-    </>
-  )
+	return (
+		<>
+			<rootContext.Provider value={rootCtx}>
+				<div id="container">
+					<div id="grid-container" style={{ display: display }}>
+						<div id="npkk">
+							<img src={Logo}></img>
+						</div>
+						{props.map((prop) => {
+							return (
+								<div id={prop.id} onClick={() => onTileClick(prop.id)}>
+									<GridTile title={prop.title}></GridTile>
+								</div>
+							);
+						})}
+					</div>
+					<ContentModal></ContentModal>
+				</div>
+			</rootContext.Provider>
+		</>
+	);
 }
 
-export default App
+export default App;
--- src/components/ContentModal.tsx
+++ src/components/ContentModal.tsx
@@ -1,32 +1,36 @@
-import { useContext } from "react"
-import './ContentModal.css'
-import { rootContext } from "../contexts/contexts"
-import props from '../contents/content'
+import { useContext } from "react";
+import "./ContentModal.css";
+import { rootContext } from "../contexts/contexts";
+import props from "../contents/content";
 
 export default function ContentModal() {
-    const rootCtx = useContext(rootContext)
-    const display = rootCtx.root ? '' : 'none'
+	const rootCtx = useContext(rootContext);
+	const display = rootCtx.root ? "" : "none";
 
-    function handleClose() {
-        rootCtx.setRoot('')
-    }
+	function handleClose() {
+		rootCtx.setRoot("");
+	}
 
-    return (
-        <>
-            <div id="contentmodal__modal" style={{ display: display }}>
-                <div id="contentmodal__body">
-                    <div id="contentmodal__content">
-                        {
-                            props.filter((prop) => prop.id === rootCtx.root).map(prop => {
-                                return prop.content
-                            })
-                        }
-                    </div>
-                    <button id="contentmodal__close" onClick={() => handleClose()}>
-                        Close
-                    </button>
-                </div>
-            </div>
-        </>
-    )
+	return (
+		<>
+			<div id="contentmodal__modal" style={{ display: display }}>
+				<div id="contentmodal__body">
+					<div id="contentmodal__content">
+						{props
+							.filter((prop) => prop.id === rootCtx.root)
+							.map((prop) => {
+								return prop.content;
+							})}
+					</div>
+					<button
+						type="button"
+						id="contentmodal__close"
+						onClick={() => handleClose()}
+					>
+						Back
+					</button>
+				</div>
+			</div>
+		</>
+	);
 }
